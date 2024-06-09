23a0b7b798f8f71e3808b3d1affaffee2209c4eb
ALexTik142 <hitstoun@gmail.com>
2024-06-09 21:10:24 +0600

    Added new model Status
    Added new data table status
    Added field in main table status_id

--- main/models.py
+++ main/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
-class Task(models.Model):   #Модель Task
+class Status(models.Model):
+    name_status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name_status
+
+
+class Task(models.Model):
     title = models.CharField('Название', max_length=50)
     task = models.TextField('Описание')
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.title
